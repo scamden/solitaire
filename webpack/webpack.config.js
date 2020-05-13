bc776d434734335a5170da337b92710d6d30fba8
Anthony Liatsis <anthony.liatsis@gmail.com>
2020-05-12 22:50:29 -0700

    chore(browser): disable broken favicon and remove env from web page title

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -20,44 +20,44 @@ function getApiStageVariables(apiStage) {
     case 'prod':
       return {
         __API_BASE_URL__: '"https://api.creditiq.com"',
-          __SOCKET_BASE_URL__: '"https://socket.creditiq.com"',
-          __SPLIT_API_KEY__: '"45kigfkjph2hipkm0p92l1si72og3ooov32j"',
-          __SENTRY_URL__: '"https://1cd04e434335429085e3ab6780e2b77a@sentry.io/148147"',
-          __NEW_RELIC_APP_ID__: '"51898835"',
+        __SOCKET_BASE_URL__: '"https://socket.creditiq.com"',
+        __SPLIT_API_KEY__: '"45kigfkjph2hipkm0p92l1si72og3ooov32j"',
+        __SENTRY_URL__: '"https://1cd04e434335429085e3ab6780e2b77a@sentry.io/148147"',
+        __NEW_RELIC_APP_ID__: '"51898835"',
       }
-      case 'staging':
-        return {
-          __API_BASE_URL__: '"https://api.staging.creditiq.com"',
-            __SOCKET_BASE_URL__: '"https://socket.staging.creditiq.com"',
-            __SPLIT_API_KEY__: '"qrseg78md8q2go6gq37usd02s4cutptk07qn"',
-            __SENTRY_URL__: '"https://6f7ddaa1250d46c1bc523d538d6c2726@sentry.io/148097"',
-            __NEW_RELIC_APP_ID__: '"51902382"',
-        }
+    case 'staging':
+      return {
+        __API_BASE_URL__: '"https://api.staging.creditiq.com"',
+        __SOCKET_BASE_URL__: '"https://socket.staging.creditiq.com"',
+        __SPLIT_API_KEY__: '"qrseg78md8q2go6gq37usd02s4cutptk07qn"',
+        __SENTRY_URL__: '"https://6f7ddaa1250d46c1bc523d538d6c2726@sentry.io/148097"',
+        __NEW_RELIC_APP_ID__: '"51902382"',
+      }
 
-        case 'dev':
-          return {
-            __API_BASE_URL__: '"https://api.dev.creditiq.com"',
-              __SOCKET_BASE_URL__: '"https://socket.dev.creditiq.com"',
-              __SPLIT_API_KEY__: '"4qd9s8hum9s4jereec5b2218rfg7i7b405nd"',
-              __SENTRY_URL__: '""',
-              __NEW_RELIC_APP_ID__: '""',
-          }
-          case 'local':
-            return {
-              __API_BASE_URL__: '"http://localhost:3000"',
-                __SOCKET_BASE_URL__: '"http://localhost:8888"',
-                __SPLIT_API_KEY__: '"4qd9s8hum9s4jereec5b2218rfg7i7b405nd"',
-                __SENTRY_URL__: '""',
-                __NEW_RELIC_APP_ID__: '""',
-            }
-            default:
-              return {
-                __API_BASE_URL__: '""',
-                  __SOCKET_BASE_URL__: '""',
-                  __SPLIT_API_KEY__: '"4qd9s8hum9s4jereec5b2218rfg7i7b405nd"',
-                  __SENTRY_URL__: '""',
-                  __NEW_RELIC_APP_ID__: '""',
-              }
+    case 'dev':
+      return {
+        __API_BASE_URL__: '"https://api.dev.creditiq.com"',
+        __SOCKET_BASE_URL__: '"https://socket.dev.creditiq.com"',
+        __SPLIT_API_KEY__: '"4qd9s8hum9s4jereec5b2218rfg7i7b405nd"',
+        __SENTRY_URL__: '""',
+        __NEW_RELIC_APP_ID__: '""',
+      }
+    case 'local':
+      return {
+        __API_BASE_URL__: '"http://localhost:3000"',
+        __SOCKET_BASE_URL__: '"http://localhost:8888"',
+        __SPLIT_API_KEY__: '"4qd9s8hum9s4jereec5b2218rfg7i7b405nd"',
+        __SENTRY_URL__: '""',
+        __NEW_RELIC_APP_ID__: '""',
+      }
+    default:
+      return {
+        __API_BASE_URL__: '""',
+        __SOCKET_BASE_URL__: '""',
+        __SPLIT_API_KEY__: '"4qd9s8hum9s4jereec5b2218rfg7i7b405nd"',
+        __SENTRY_URL__: '""',
+        __NEW_RELIC_APP_ID__: '""',
+      }
   }
 }
 
@@ -87,11 +87,11 @@ module.exports = ({
     sourceMapFilename: '[file].map',
     libraryTarget: 'commonjs2'
   } : {
-    path: path.resolve(distPath, `${isDemo ? 'demo' : 'main'}`),
-    publicPath: '/',
-    filename: '[name].[chunkhash].js',
-    sourceMapFilename: '[file].map'
-  };
+        path: path.resolve(distPath, `${isDemo ? 'demo' : 'main'}`),
+        publicPath: '/',
+        filename: '[name].[chunkhash].js',
+        sourceMapFilename: '[file].map'
+      };
 
   let __TITLE_ENV__ = '"';
   if (isDev) {
@@ -103,7 +103,9 @@ module.exports = ({
   if (isDemo) {
     __TITLE_ENV__ += ' - Demo';
   }
-  if (!!apiStage) {
+
+  const isProdNotDemo = apiStage === 'prod';
+  if (!!apiStage && !isProdNotDemo) {
     __TITLE_ENV__ += ` - ${_capitalize(apiStage)}`;
     if (isDev) {
       __TITLE_ENV__ += ' API';
@@ -111,13 +113,11 @@ module.exports = ({
   }
   __TITLE_ENV__ += '"';
 
-  const isProdNotDemo = apiStage === 'prod';
-
   const plugins = [
     // isDev && new BundleAnalyzerPlugin() || (() => {}),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
 
-    !isDemo ? new ForkTsCheckerWebpackPlugin() : (() => {}),
+    !isDemo ? new ForkTsCheckerWebpackPlugin() : (() => { }),
 
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(isDev ? 'development' : 'production'), // Tells React to build in either dev or prod modes. https://facebook.github.io/react/downloads.html (See bottom)
@@ -209,94 +209,94 @@ module.exports = ({
 
   const module = {
     rules: [{
-        test: /\.tsx?$/,
-        use: [{
-          loader: 'ts-loader',
-          options: {
-            transpileOnly: !needsCompile, // checking is done by the ForkTsCheckerWebpackPlugin
-            compilerOptions: {
-              outDir: './'
-            }
-          }
-        }]
-      },
-      {
-        test: /\.js$/,
-        enforce: 'pre',
-        use: ["source-map-loader"]
-      },
-      {
-        test: /\.eot(\?v=\d+.\d+.\d+)?$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            'name': 'assets/fonts/[name].[ext]'
+      test: /\.tsx?$/,
+      use: [{
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: !needsCompile, // checking is done by the ForkTsCheckerWebpackPlugin
+          compilerOptions: {
+            outDir: './'
           }
         }
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 10000,
-            mimetype: 'application/font-woff',
-            name: 'assets/fonts/[name].[ext]'
-          }
-        }]
-      },
-      {
-        test: /\.ttf(\?v=\d+.\d+.\d+)?$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 10000,
-            mimetype: 'application/octet-stream',
-            name: 'assets/fonts/[name].[ext]'
-          }
-        }]
-      },
-      {
-        test: /\.svg(\?v=\d+.\d+.\d+)?$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 10000,
-            mimetype: 'image/svg+xml',
-            name: 'assets/fonts/[name].[ext]'
-          }
-        }],
-      },
-      {
-        test: /\.(jpe?g|png|gif|pdf)$/i,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: 'assets/images/[name].[ext]'
-          }
-        }]
-      },
-      {
-        test: /\.ico$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: 'assets/icons/[name].[ext]'
-          }
-        }]
-      },
-      {
-        test: (absPath) => /\.scss$/.test(absPath) && !globalSassRegex.test(absPath),
-        use: extractTextOptionsNonGlobal
-      },
-      {
-        test: globalSassRegex,
-        use: extractTextOptionsGlobal
-      },
-      {
-        test: /\.css$/,
-        use: extractTextOptionsCss
+      }]
+    },
+    {
+      test: /\.js$/,
+      enforce: 'pre',
+      use: ["source-map-loader"]
+    },
+    {
+      test: /\.eot(\?v=\d+.\d+.\d+)?$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          'name': 'assets/fonts/[name].[ext]'
+        }
       }
+    },
+    {
+      test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      use: [{
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          mimetype: 'application/font-woff',
+          name: 'assets/fonts/[name].[ext]'
+        }
+      }]
+    },
+    {
+      test: /\.ttf(\?v=\d+.\d+.\d+)?$/,
+      use: [{
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          mimetype: 'application/octet-stream',
+          name: 'assets/fonts/[name].[ext]'
+        }
+      }]
+    },
+    {
+      test: /\.svg(\?v=\d+.\d+.\d+)?$/,
+      use: [{
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          mimetype: 'image/svg+xml',
+          name: 'assets/fonts/[name].[ext]'
+        }
+      }],
+    },
+    {
+      test: /\.(jpe?g|png|gif|pdf)$/i,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          name: 'assets/images/[name].[ext]'
+        }
+      }]
+    },
+    {
+      test: /\.ico$/,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          name: 'assets/icons/[name].[ext]'
+        }
+      }]
+    },
+    {
+      test: (absPath) => /\.scss$/.test(absPath) && !globalSassRegex.test(absPath),
+      use: extractTextOptionsNonGlobal
+    },
+    {
+      test: globalSassRegex,
+      use: extractTextOptionsGlobal
+    },
+    {
+      test: /\.css$/,
+      use: extractTextOptionsCss
+    }
     ]
   };
 
@@ -336,30 +336,30 @@ module.exports = ({
   };
   if (!isDev) {
     config.externals = [{
-        ...(isLibrary && {
-          'react': 'commonjs react', // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
-          // leaving these here to make 100 percent sure they are excluded even if they aren't in peerDeps but should be handled by the package.json reading below
-          '@creditiq/credit-report-modal': 'commonjs @creditiq/credit-report-modal',
-          '@creditiq/item': 'commonjs @creditiq/item',
-          '@creditiq/loading': 'commonjs @creditiq/loading',
-          '@creditiq/split-web-client': 'commonjs @creditiq/split-web-client',
-          'global.scss': 'commonjs global.scss',
-          '_variables.scss': 'commonjs _variables.scss',
-          '_mixins.scss': 'commonjs _mixins.scss',
-          'decimal.js': 'commonjs decimal.js',
-          ...Object.keys(require('../package.json').peerDependencies || {}).reduce((externals, dep) => ({
-            ...externals,
-            [dep]: `commonjs @root/${dep}`
-          }), {})
-        }),
-        ...externals,
-      },
-      isLibrary && function (context, request, callback) {
-        if (/^lodash\//.test(request)) {
-          return callback(null, 'commonjs ' + request);
-        }
-        callback();
-      } || {}
+      ...(isLibrary && {
+        'react': 'commonjs react', // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
+        // leaving these here to make 100 percent sure they are excluded even if they aren't in peerDeps but should be handled by the package.json reading below
+        '@creditiq/credit-report-modal': 'commonjs @creditiq/credit-report-modal',
+        '@creditiq/item': 'commonjs @creditiq/item',
+        '@creditiq/loading': 'commonjs @creditiq/loading',
+        '@creditiq/split-web-client': 'commonjs @creditiq/split-web-client',
+        'global.scss': 'commonjs global.scss',
+        '_variables.scss': 'commonjs _variables.scss',
+        '_mixins.scss': 'commonjs _mixins.scss',
+        'decimal.js': 'commonjs decimal.js',
+        ...Object.keys(require('../package.json').peerDependencies || {}).reduce((externals, dep) => ({
+          ...externals,
+          [dep]: `commonjs @root/${dep}`
+        }), {})
+      }),
+      ...externals,
+    },
+    isLibrary && function (context, request, callback) {
+      if (/^lodash\//.test(request)) {
+        return callback(null, 'commonjs ' + request);
+      }
+      callback();
+    } || {}
     ];
   };
   return config;
